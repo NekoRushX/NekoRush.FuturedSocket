d670e2212353b9b8ee8bcb99cdb3e1dbb0fdcb3f
TheSnowfield <thesnowfield@moezsoft.com>
2022-03-31 18:00:28 +0800

    Add test

---
+++ NekoRush.FuturedSocket.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NekoRush.FuturedSocket.Test;
+
+public class Tests
+{
+    [Test]
+    public static async Task Test()
+    {
+        var socket = new FuturedSocket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+        {
+            if (await socket.Connect("example.com", 80))
+            {
+                // Send fake http request
+                await socket.Send(Encoding.UTF8.GetBytes
+                    ("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n"));
+
+                var buffer = new byte[1024];
+                var read = await socket.Receive(buffer);
+
+                Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, read));
+
+                socket.Dispose();
+                Assert.AreNotEqual(read, 0);
+            }
+        }
+    }
+}
